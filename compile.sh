e40adf6357d7ec07f619797e31ed92fe5ff7aa14
Alec Robinson <alecnotalex@ymail.com>
2013-02-13 18:35:52 -0500

    Changed the compile script to match the new source file.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PKG_CONFIG_PATH=/home/pi/OpenCV-2.4.3/release/unix-install/:${PKG_CONFIG_PATH}
 export LD_LIBRARY_PATH=/home/pi/OpenCV-2.4.3/release/lib
-g++ `pkg-config --cflags --libs opencv` targeting.cpp -o targeting 
+g++ `pkg-config --cflags --libs opencv` targeting2.cpp -o targeting 
